b8503193471efb9c0226c7f953eb9ce54d4e80ae
Valentine <valentineladjyn@hotmail.fr>
2025-09-02 16:22:32 +0200

    handler pour les inscriptions non testé, et pas ajouré au fichier routes.go

--- handlers/registerhandler.go
+++ handlers/registerhandler.go
@@ -1,6 +1,14 @@
 package handlers
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/Mathis-Pain/Forum/utils"
+)
+
+// Handler regroupe toutes les dépendances dont les routes ont besoin
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
@@ -8,6 +16,32 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 	passwordConfirm := r.FormValue("confirmpassword")
 
-	//Gestion d'erreur
+	var db *sql.DB
 
+	//Gestion d'erreur
+	erreur := utils.ValidName(name)
+	if erreur != nil {
+		fmt.Fprint(w, erreur)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	erreur = utils.ValidEmail(email)
+	if erreur != nil {
+		fmt.Fprint(w, erreur)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	erreur = utils.ValidPasswd(password, passwordConfirm)
+	if erreur != nil {
+		fmt.Fprint(w, erreur)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	_, err := db.Exec("INSERT INTO users(name, email, password) VALUES(?, ?, ?)", name, email, password)
+	if err != nil {
+		http.Error(w, "Erreur DB: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, "Utilisateur ajouté")
 }
