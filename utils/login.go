3c66c1fb9b025bc841e4efa3f04567aa446586b9
jcolange <jade.colanges@live.fr>
2025-09-03 10:16:09 +0200

    merge fix

--- utils/login.go
+++ utils/login.go
@@ -6,25 +6,11 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/Mathis-Pain/Forum/models"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func GetUserInfo(db *sql.DB, input string) (models.User, error) {
-	sql := `SELECT ID, password FROM users WHERE email = ? OR username = ?`
-	row := db.QueryRow(sql, input, input)
-
-	var user models.User
-	err := row.Scan(&user.ID, &user.Password)
-	if err != nil {
-		return models.User{}, err
-	}
-
-	return user, nil
-}
-
 func Authentification(db *sql.DB, login string, password string) error {
-	user, err := GetUserInfo(db, login)
+	user, err := GetUserInfoFromLogin(db, login)
 	if errors.Is(err, sql.ErrNoRows) {
 		log.Printf("<login.go l.35 : login failed, User %v not found\n", login)
 		return fmt.Errorf("incorrect password or username")
