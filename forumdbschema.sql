fae4537e3e71638073261837735eda9c4e3fbc7a
mpain <mathis.pain76@gmail.com>
2025-09-16 11:32:05 +0200

    database update like/dislike

--- forumdbschema.sql
+++ forumdbschema.sql
@@ -1,63 +1,63 @@
 -- Table des rôles
-CREATE TABLE roles (
+CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE
 );
 
 -- Table des utilisateurs
-CREATE TABLE users (
+CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     profilpic TEXT DEFAULT 'static/noprofilpic.png',
     role_id INTEGER NOT NULL DEFAULT 1,
-    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
 
 -- Table des catégories
-CREATE TABLE categories (
+CREATE TABLE category (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE,
     description TEXT
 );
 
 -- Table des topics
-CREATE TABLE topics (
+CREATE TABLE topic (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     created_at TEXT DEFAULT (CURRENT_TIMESTAMP),
     user_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
--- Table des messages
-CREATE TABLE messages (
+-- Table des message
+CREATE TABLE message (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     topic_id INTEGER NOT NULL,
     content TEXT NOT NULL,
     created_at TEXT DEFAULT (CURRENT_TIMESTAMP),
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (topic_id) REFERENCES topic(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
--- Table des likes
-CREATE TABLE likes (
+-- Table des like
+CREATE TABLE like (
     user_id INTEGER NOT NULL,
     message_id INTEGER NOT NULL,
     PRIMARY KEY (user_id, message_id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (message_id) REFERENCES message(id) ON DELETE CASCADE
 );
 
--- Table des dislikes
-CREATE TABLE dislikes (
+-- Table des dislike
+CREATE TABLE dislike (
     user_id INTEGER NOT NULL,
     message_id INTEGER NOT NULL,
     PRIMARY KEY (user_id, message_id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (message_id) REFERENCES message(id) ON DELETE CASCADE
 );
