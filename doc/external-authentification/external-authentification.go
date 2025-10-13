b004f2e6b45363905feba53a93c3ad0a16d105a5
mpain <mathis.pain76@gmail.com>
2025-10-13 10:59:08 +0200

    externalauth

---
+++ doc/external-authentification/external-authentification.go
@@ -0,0 +1,36 @@
+package externalauthentification
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+func ExternalAuth() {
+	// Charge le fichier .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erreur chargement .env : %v", err)
+	}
+
+	// Lire les variables
+	clientID := os.Getenv("GOOGLE_CLIENT_ID")
+	clientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	redirectURL := os.Getenv("GOOGLE_REDIRECT_URL")
+
+	fmt.Println("Client ID :", clientID)
+	fmt.Println("Redirect URL :", redirectURL)
+
+	// Ici tu peux continuer avec la configuration OAuth2 Google
+}
+
+var googleOAuthConfig = &oauth2.Config{
+	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+	RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+	Endpoint:     google.Endpoint,
+}
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.24.2
 require golang.org/x/crypto v0.41.0
 
 require github.com/mattn/go-sqlite3 v1.14.32
+
+require github.com/joho/godotenv v1.5.1 // indirect
